c8b683d49e756812075ecbf010817e0660d85819
VerifiedFans <troy@knoxconcepts.com>
2025-05-20 14:06:04 -0400

    Update push-schema.js

--- pages/api/push-schema.js
+++ pages/api/push-schema.js
@@ -1,1 +1,16 @@
+import { exec } from 'child_process';
 
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  exec('npx prisma db push', (error, stdout, stderr) => {
+    if (error) {
+      console.error(stderr);
+      return res.status(500).json({ error: stderr });
+    }
+    console.log(stdout);
+    res.status(200).json({ message: stdout });
+  });
+}
